1b7c807b9c6b90cbf4f0f1f3e1cb36c0f6baefe8
mattdevoti1 <mattdevoti1@optonline.net>
2024-01-03 11:25:37 -0500

    Set up down registry DB in mongodb

--- src/main/java/com/nook/TownRegistry/model/citizen/ResidentResponse.java
+++ src/main/java/com/nook/TownRegistry/model/citizen/ResidentResponse.java
@@ -3,7 +3,11 @@ package com.nook.TownRegistry.model.citizen;
 import com.nook.TownRegistry.model.citizen.citizenEnums.Gender;
 import com.nook.TownRegistry.model.citizen.citizenEnums.Species;
 import com.nook.TownRegistry.model.songs.Song;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class ResidentResponse {
     private String name;
     private Gender gender;
