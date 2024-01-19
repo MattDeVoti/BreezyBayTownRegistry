6e1ae49df848971b781a8a294aabc02b91f11117
mattdevoti1 <mattdevoti1@optonline.net>
2024-01-18 20:38:22 -0500

    Bulletin messages Time to Live is set and they will dissapear after a set amount of time

--- src/main/java/com/nook/TownRegistry/model/bulletinBoard/BulletinBoardPost.java
+++ src/main/java/com/nook/TownRegistry/model/bulletinBoard/BulletinBoardPost.java
@@ -5,17 +5,12 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @RequiredArgsConstructor
 @Getter
 @Setter
 @Document(collection = "bulletinBoardPost")
 public class BulletinBoardPost {
-    // TODO: Make expiration time configurable
-//    @Value("${townRegistry.bulletinBoard.time.until.expiration:5}")
-//    private long expirationTime = 5;
-
     @Id
     private final String messageId;
 
@@ -24,7 +19,6 @@ public class BulletinBoardPost {
     private final String message;
     private final String townName;
     private final String citizenName;
-
 
     @Indexed(name = "deleteAt", expireAfterSeconds=300)
     private final LocalDateTime createdAt;
