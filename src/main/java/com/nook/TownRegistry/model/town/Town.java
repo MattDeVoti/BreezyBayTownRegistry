aa0dd9670640cbdc55c6a269efcd683a37ac907b
mattdevoti1 <mattdevoti1@optonline.net>
2024-01-25 11:26:53 -0500

    Added @EqualsAndHashCode annotation to model methods

--- src/main/java/com/nook/TownRegistry/model/town/Town.java
+++ src/main/java/com/nook/TownRegistry/model/town/Town.java
@@ -3,6 +3,7 @@ package com.nook.TownRegistry.model.town;
 import com.nook.TownRegistry.model.museum.Museum;
 import com.nook.TownRegistry.model.town.townEnums.GeneralStore;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
@@ -11,6 +12,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Getter
 @Setter
 @Data
+@EqualsAndHashCode
 @Document(collection = "town")
 public class Town {
     @Id
